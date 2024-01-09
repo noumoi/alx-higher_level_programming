dbd651ff9fb639d02531731a80ad9f4061cb63a5
noumoiclear <holawaleh.software@gmail.com>
2024-01-09 07:37:20 +0100

    2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,11 +1,33 @@
-#!//usr/bin/python3
+#!/bin/bash
 import random
 number = random.randint(-10000, 10000)
-last_digit = abs(number) % 10 if number >= 0 else -abs(number) % 10
-print("Last digit of", number, "is", last_digit, end=" ")
-if last_digit > 5:
-    print("and is greater than 5")
-elif last_digit == 0:
-    print("and is 0")
-else:
-    print("and is less than 6 and not 0")
+lastdigs = number % 10
+if number >= 0:
+    if lastdigs > 5:
+        print("Last digit of {} is {} and\
+    is greater than 5".format(number, lastdigs))
+    elif lastdigs == 0:
+        print("Last digit of {} is {} and\
+is 0".format(number, lastdigs))
+    
+    else:
+    
+        print("Last digit of {} is {} and\
+
+  is less than 6 and not 0".format(number, lastdigs))
+
+    else:
+    
+        lastdigsneg = -number
+  
+        if lastdigsneg % 10 == 0:
+        
+            print("Last digit of {} is {} and\
+            
+  is 0".format(number, -(lastdisgneg % 10)))
+  
+        else:
+        
+            print("Last digit of {} is {} and\
+            
+  is less than 6 and not 0".format(number, -(lastdigsneg % 10)))
