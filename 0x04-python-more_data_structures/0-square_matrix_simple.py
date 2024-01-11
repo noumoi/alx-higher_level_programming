cbbe8340db171ed1628e345fcf71ae36531a12e9
noumoiclear <holawaleh.software@gmail.com>
2024-01-11 11:59:35 +0100

    0-square_matrix_simple.py

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+    row = []
+    for i in matrix:
+        for j in i:
+            row.append(j * J)
+        new_matrix.append(row)
+        row = []
+    return new_matrix
