f35cf67f245fe0132f352f59572ecabd54d1b630
noumoiclear <holawaleh.software@gmail.com>
2024-01-11 22:17:13 +0100

    1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,8 +1,8 @@
- #!/usr/bin/python3
- def search_replace(my_list, search, replace):
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
     new_list = []
     for i, j in enumerate(my_list):
         if j == search:
-               j = replace
+            j = replace
         new_list.append(j)
     return new_list
