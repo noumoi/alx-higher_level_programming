3490a9f71e0ca6aaf01f517b893fcfeba9ed0797
noumoi <holawaleh.software@gmail.com>
2024-03-10 01:51:26 +0100

    1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,7 +3,7 @@ def element_at(my_list, idx):
     for i in my_list:
         if idx < 0:
             return Nonei
-        elif idx > len(my_list)-1:
+        elif idx > len(my_list):
             return None
         else:
             return my_list[idx]
