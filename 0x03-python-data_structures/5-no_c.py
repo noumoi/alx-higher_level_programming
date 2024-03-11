c7be01698e8afdf12e527f39370d7dc1dc7bbf17
noumoi <holawaleh.software@gmail.com>
2024-03-11 09:24:43 +0100

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python3
 def no_c(my_string):
-    new_string = ""
-    for char in my_string:
-        if char != 'c' and char != 'C':
-            new_string += char
+    new_string = ''
+    for k in my_string:
+        if k in ['c', 'C']:
+            continue
+        new_string = new_string + k
     return new_string
