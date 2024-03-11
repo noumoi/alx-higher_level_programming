3b9d079fc3c4070b0dee97745224efb9bafd992f
noumoi <holawaleh.software@gmail.com>
2024-03-11 09:27:21 +0100

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 def no_c(my_string):
     new_string = ''
-    for k in my_string:
-        if k in ['c', 'C']:
+    for j in my_string:
+        if j in ['c', 'C']:
             continue
-        new_string = new_string + k
+        new_string = new_string + j
     return new_string
